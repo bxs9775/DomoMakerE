bf5c8cb0e5aa033950c72233931f3ad7b990a1b9
BRIAN SANDON (RIT Student) <bxs9775@ad.rit.edu>
2018-03-09 15:20:42 -0500

    Set up base app.js, router.js, and controller code * Log-in page and sign-in page are served correctly

---
+++ server/app.js
@@ -0,0 +1,47 @@
+// imports
+const path = require('path');
+const express = require('express');
+const compression = require('compression');
+const favicon = require('serve-favicon');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const expressHandlebars = require('express-handlebars');
+
+// sets the port for the server to use
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+// sets the url for the connected database
+const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
+
+// trys to connect to the database
+mongoose.connect(dbURL, (err) => {
+  if (err) {
+    console.log('Could not connect to database');
+    throw err;
+  }
+});
+
+// get the routers for the program
+const router = require('./router.js');
+
+const app = express();
+app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
+app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+app.use(compression());
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.set('views', `${__dirname}/../views`);
+app.use(cookieParser());
+
+router(app);
+
+app.listen(port, (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log(`Listening on port ${port}`);
+});
---
+++ server/controllers/Account.js
@@ -0,0 +1,30 @@
+const models = require('../models');
+
+const Account = models.Account;
+
+const loginPage = (req, res) => {
+  res.render('login');
+};
+
+const signupPage = (req, res) => {
+  res.render('signup');
+};
+
+const logout = (req, res) => {
+  res.redirect('/');
+};
+
+const login = (req, res) => {
+  // TODO: implement function
+};
+
+const signup = (req, res) => {
+  // TODO: implement function
+};
+
+// exports
+module.exports.loginPage = loginPage;
+module.exports.login = login;
+module.exports.logout = logout;
+module.exports.signupPage = signupPage;
+module.exports.signup = signup;
