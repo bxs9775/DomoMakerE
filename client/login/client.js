dd7d618c69aa3701ee4bee9ec0c9ecee19b01bc0
bxs9775 <bxs9775@rit.edu>
2018-03-23 17:09:10 -0400

    Transitioning to React (Incomplete) part 3

--- client/login/client.js
+++ client/login/client.js
@@ -1,4 +1,10 @@
-//Handles the login process
+const getToken = () => {
+  sendAjax('GET','/getToken', null, (result) => {
+    setup(result.csrfToken);
+  })
+}
+
+// Handles the login process
 const handleLogin = (e) => {
   e.preventDefault();
   
@@ -16,7 +22,7 @@ const handleLogin = (e) => {
   return false;
 };
 
-//Handles the signup process
+// Handles the signup process
 const handleSignup = (e) => {
   e.preventDefault();
   
@@ -36,3 +42,84 @@ const handleSignup = (e) => {
   
   return false;
 };
+
+// Creates the login screen for the client
+const LoginWindow = (props) => {
+  return (
+  <form id="loginForm" name="loginForm"
+    onSubmit={handleLogin}
+    action="/login"
+    method="POST"
+    className="mainForm"
+    >
+      <label htmlFor="username">Username: </label>
+      <input id="user" type="text" name="username" placeholder="username"/>
+      <label htmlFor="pass">Password: </label>
+      <input id="pass" type="text" name="pass" placeholder="password"/>
+      <input type="hidden" name="_csrf" value={props.csrf}/>
+      <input className="formSubmit" type="submit" value="Sign In" />
+    </form>
+  );
+};
+
+// Creates the signup screen for the client
+const SignupWindow = (props) => {
+  return (
+  <form id="signupForm" name="signupForm"
+    onSubmit={handleSignup}
+    action="/signup"
+    method="POST"
+    className="mainForm"
+    >
+      <label htmlFor="username">Username: </label>
+      <input id="user" type="text" name="username" placeholder="username"/>
+      <label htmlFor="pass">Password: </label>
+      <input id="pass" type="text" name="pass" placeholder="password"/>
+      <label htmlFor="pass2">Password: </label>
+      <input id="pass2" type="text" name="pass2" placeholder="retype password"/>
+      <input type="hidden" name="_csrf" value={props.csrf}/>
+      <input className="formSubmit" type="submit" value="Sign Up" />
+    </form>
+  );
+};
+
+// Renders the Login Window
+const createLoginWindow = (csrf) => {
+  ReactDOM.render(
+    <LoginWindow csrf={csrf} />,
+    document.querySelector("content")
+  );
+};
+
+// Renders the Signup Window
+const createSignupWindow = (csrf) => {
+  ReactDOM.render(
+    <SignupWindow csrf={csrf} />,
+    document.querySelector("content")
+  );
+};
+
+//Sets up event listeners and takes the user to the login 'page'
+const setup = (csrf) => {
+  const signupButton = document.querySelector("#signupButton");
+  const loginButton = document.querySelector("#loginButton");
+  
+  signupButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    createSignupWindow(csrf);
+    return false;
+  });
+  
+  loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    createLoginWindow(csrf);
+    return false;
+  });
+  
+  createLoginWindow(csrf); //default view
+};
+
+//Runs setup when the document is opened.
+$(document).ready(function() {
+  getToken();
+});
---
+++ hosted/loginBundle.js
@@ -0,0 +1,170 @@
+'use strict';
+
+var getToken = function getToken() {
+  sendAjax('GET', '/getToken', null, function (result) {
+    setup(result.csrfToken);
+  });
+};
+
+// Handles the login process
+var handleLogin = function handleLogin(e) {
+  e.preventDefault();
+
+  $("#domoMessage").animate({ width: 'hide' }, 350);
+
+  if ($("#user").val() == '' || $("#pass").val() == '') {
+    handleError("RAWR! Username or password is empty");
+    return false;
+  }
+
+  console.log($("input[name=_csrf]").val());
+
+  sendAjax('POST', $("#loginForm").attr("action"), $("loginForm").serialize(), redirect);
+
+  return false;
+};
+
+// Handles the signup process
+var handleSignup = function handleSignup(e) {
+  e.preventDefault();
+
+  $("#domoMessage").animate({ width: 'hide' }, 350);
+
+  if ($("user").val() == '' || $("#pass").val() == '' || $("#pass2").val() == '') {
+    handleError("RAWR! All fields are required");
+    return false;
+  }
+
+  if ($("#pass").val() !== $("pass2").val()) {
+    handleError("RAWR! Passwords do not match");
+    return false;
+  }
+
+  sendAjax('POST', $("signupForm").attr("action"), $("#signupForm").serialize(), redirect);
+
+  return false;
+};
+
+// Creates the login screen for the client
+var LoginWindow = function LoginWindow(props) {
+  return React.createElement(
+    'form',
+    { id: 'loginForm', name: 'loginForm',
+      onSubmit: handleLogin,
+      action: '/login',
+      method: 'POST',
+      className: 'mainForm'
+    },
+    React.createElement(
+      'label',
+      { htmlFor: 'username' },
+      'Username: '
+    ),
+    React.createElement('input', { id: 'user', type: 'text', name: 'username', placeholder: 'username' }),
+    React.createElement(
+      'label',
+      { htmlFor: 'pass' },
+      'Password: '
+    ),
+    React.createElement('input', { id: 'pass', type: 'text', name: 'pass', placeholder: 'password' }),
+    React.createElement('input', { type: 'hidden', name: '_csrf', value: props.csrf }),
+    React.createElement('input', { className: 'formSubmit', type: 'submit', value: 'Sign In' })
+  );
+};
+
+// Creates the signup screen for the client
+var SignupWindow = function SignupWindow(props) {
+  return React.createElement(
+    'form',
+    { id: 'signupForm', name: 'signupForm',
+      onSubmit: handleSignup,
+      action: '/signup',
+      method: 'POST',
+      className: 'mainForm'
+    },
+    React.createElement(
+      'label',
+      { htmlFor: 'username' },
+      'Username: '
+    ),
+    React.createElement('input', { id: 'user', type: 'text', name: 'username', placeholder: 'username' }),
+    React.createElement(
+      'label',
+      { htmlFor: 'pass' },
+      'Password: '
+    ),
+    React.createElement('input', { id: 'pass', type: 'text', name: 'pass', placeholder: 'password' }),
+    React.createElement(
+      'label',
+      { htmlFor: 'pass2' },
+      'Password: '
+    ),
+    React.createElement('input', { id: 'pass2', type: 'text', name: 'pass2', placeholder: 'retype password' }),
+    React.createElement('input', { type: 'hidden', name: '_csrf', value: props.csrf }),
+    React.createElement('input', { className: 'formSubmit', type: 'submit', value: 'Sign Up' })
+  );
+};
+
+// Renders the Login Window
+var createLoginWindow = function createLoginWindow(csrf) {
+  ReactDOM.render(React.createElement(LoginWindow, { csrf: csrf }), document.querySelector("content"));
+};
+
+// Renders the Signup Window
+var createSignupWindow = function createSignupWindow(csrf) {
+  ReactDOM.render(React.createElement(SignupWindow, { csrf: csrf }), document.querySelector("content"));
+};
+
+//Sets up event listeners and takes the user to the login 'page'
+var setup = function setup(csrf) {
+  var signupButton = document.querySelector("#signupButton");
+  var loginButton = document.querySelector("#loginButton");
+
+  signupButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    createSignupWindow(csrf);
+    return false;
+  });
+
+  loginButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    createLoginWindow(csrf);
+    return false;
+  });
+
+  createLoginWindow(csrf); //default view
+};
+
+//Runs setup when the document is opened.
+$(document).ready(function () {
+  getToken();
+});
+"use strict";
+
+//Handles error by displaying it on the page.
+var handleError = function handleError(message) {
+  $("#errorMessage").text(message);
+  $("domoMessage").animate({ width: 'toggle' }, 350);
+};
+
+//Redirects the client to the given page.
+var redirect = function redirect(response) {
+  $("#domoMessage").animate({ width: 'hide' }, 350);
+  window.location = response.redirect;
+};
+
+//Handles AJAX calls to the server
+var sendAjax = function sendAjax(type, action, data, success) {
+  $.ajax({
+    cache: false,
+    type: type,
+    url: action,
+    data: data,
+    dataType: "json",
+    success: success,
+    error: function error(xhr, status, _error) {
+      var messageObj = JSON.parse(xhr.responseText);
+      handleError(messageObj.error);
+    }
+  });
+};
